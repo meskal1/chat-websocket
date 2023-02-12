608da210172ced3f11b2031c985330ab047fcb4d
Daniil <meskal1@rambler.ru>
2023-02-12 22:36:00 +0300

    build(root): create simple chat

--- README.md
+++ README.md
@@ -1,6 +1,46 @@
-# Getting Started with Create React App
+# Stream Chat Team Example App
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+Increase engagement in your community or replace email for your team with chat in your app. Stream’s chat SDK allows you to build anything from a simple chat to your own Slack competitor. Powerful slash commands, Giphy integration and message attachments provide the building blocks of an engaging chat experience.
+
+- Giphy, emoticons and File Attachments
+- Threads keep the discussion organized
+- Direct and Group Chats
+- Link previews help users share information
+
+
+**Quick Links**
+
+- [Component Reference](https://getstream.github.io/stream-chat-react/)
+- [Register](https://getstream.io/chat/trial/) to get an API key for Stream Chat
+- [React Chat Tutorial](https://getstream.io/chat/react-chat/tutorial/)
+- [Chat UI Kit](https://getstream.io/chat/ui-kit/)
+
+# Getting Started
+
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).\
+
+# Development process
+
+1. Clone this repository
+2. `cd team-ts && yarn install`
+3. Copy the `.env-example` file into `.env.development` file and fill in the details
+4. `yarn start`
+
+# Release process
+
+Current release process is quite manual. Until we automate it, please follow the following procedure for creating a build and a deployment to the production environment.\
+**Note**: This demo is currently hosted on GitHub Pages.
+
+1. Clean the working directory of your current branch
+    1. You can `git stash` your current work-in-progress
+2. Copy the `.env-example` file into `.env.production` and fill in just the following parameter:
+    1. `REACT_APP_STREAM_KEY=dz5f4d5kzrue`
+3. `cd .. && yarn build:t`
+    1. This command will create a production optimized bundle and will copy it to the `/docs/team` directory
+    2. Please make sure to smoke-test the demo locally before proceeding with the next step
+4. Commit the changes (minified bundle) to your branch and open a PR towards `master`
+5. Once your PR is merged, Github will automatically initiate the deployment process to Github Pages.
+6. Once the deployment process is finished, you can see your changes live on the [main website](https://getstream.io/chat/demos/team/).
 
 ## Available Scripts
 
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,103 @@
-function App() {
-  return <div>123</div>
+import { useEffect } from 'react'
+
+import { StreamChat } from 'stream-chat'
+import { Chat, enTranslations, Streami18n } from 'stream-chat-react'
+import Cookies from 'universal-cookie'
+
+// import { getRandomImage } from './assets';
+import Auth from './Auth'
+import { useChecklist } from './ChecklistTasks'
+import { ChannelContainer } from './components/ChannelContainer/ChannelContainer'
+import { Sidebar } from './components/Sidebar/Sidebar'
+import { WorkspaceController } from './context/WorkspaceController'
+import type { StreamChatType } from './types'
+
+const cookies = new Cookies()
+const authToken = cookies.get('token')
+
+const urlParams = new URLSearchParams(window.location.search)
+
+const apiKey = urlParams.get('apikey') || 'hb9ywaasq4wt'
+// const user = urlParams.get('user') || '1234237';
+const theme = urlParams.get('theme') || 'light'
+// const userToken = urlParams.get('user_token') || 'hsea2a8jrz7v883awy6gzfe2q2en8yqgmcj68fsatnbm7bjc6jmjs34c2dxuaac7';
+// const userToken = urlParams.get('user_token') || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiMTIzNDIzNyJ9.tJv6ewZWxkAHWGSY_hgFB4d82LTjgYcDZdRwOHnAmj8';
+const targetOrigin = '*'
+// const targetOrigin = 'https://server-six-pearl.vercel.app/'
+// const targetOrigin = urlParams.get('target_origin') || 'http://localhost:5000/auth'
+// const targetOrigin = urlParams.get('target_origin') || process.env.REACT_APP_TARGET_ORIGIN;
+
+const i18nInstance = new Streami18n({
+  language: 'en',
+  translationsForLanguage: {
+    ...enTranslations,
+  },
+})
+
+const client = StreamChat.getInstance<any>(apiKey!, {
+  enableInsights: true,
+  enableWSFallback: true,
+})
+
+// client.connectUser({ id: user!, name: user, image: getRandomImage() }, userToken);
+if (authToken) {
+  client.connectUser(
+    {
+      id: cookies.get('userId'),
+      name: cookies.get('username'),
+      fullName: cookies.get('fullName'),
+      image: cookies.get('avatarURL'),
+      hashedPassword: cookies.get('hashedPassword'),
+      phoneNumber: cookies.get('phoneNumber'),
+    },
+    authToken
+  )
+}
+//  export type StreamChatType = {
+// 	attachmentType: TeamAttachmentType;
+// 	channelType: TeamChannelType;
+// 	commandType: TeamCommandType;
+// 	eventType: TeamEventType;
+// 	messageType: TeamMessageType;
+// 	reactionType: TeamReactionType;
+// 	userType: TeamUserType;
+//  };
+
+const App = () => {
+  useChecklist({ chatClient: client, targetOrigin: targetOrigin! })
+
+  useEffect(() => {
+    const handleColorChange = (color: string) => {
+      const root = document.documentElement
+
+      if (color.length && color.length === 7) {
+        root.style.setProperty('--primary-color', `${color}E6`)
+        root.style.setProperty('--primary-color-alpha', `${color}1A`)
+      }
+    }
+
+    window.addEventListener('message', event => handleColorChange(event.data))
+
+    return () => {
+      client.disconnectUser()
+      window.removeEventListener('message', event => handleColorChange(event.data))
+    }
+  }, [])
+
+  if (!authToken) return <Auth />
+
+  return (
+    <>
+      <div className="app__wrapper str-chat">
+        <Chat {...{ client, i18nInstance }} theme={`team ${theme}`}>
+          <WorkspaceController>
+            <Sidebar />
+            <ChannelContainer />
+          </WorkspaceController>
+        </Chat>
+      </div>
+    </>
+  )
 }
 
 export default App
