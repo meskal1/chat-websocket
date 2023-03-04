640b794577638cec79b5ac04b1344243aaa033ca
Daniil <meskal1@rambler.ru>
2023-03-04 12:46:41 +0300

    refactor(src): refactor after store added

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,20 @@
-import { createRoot } from 'react-dom/client'
+import './index.scss'
+import './analytics/analytics'
+import './utils/setWindowHeight'
 
-import App from './App'
+import * as ReactDOMClient from 'react-dom/client'
+import { Provider } from 'react-redux'
+import { HashRouter } from 'react-router-dom'
 
-import './styles/index.scss'
+import App from './app/App'
+import { store } from './app/store'
 
-createRoot(document.getElementById('root')!).render(<App />)
+const root = ReactDOMClient.createRoot(document.getElementById('root') as HTMLElement)
+
+root.render(
+  <Provider store={store}>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </Provider>
+)
